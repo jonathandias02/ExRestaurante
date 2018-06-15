c915487375df56ab4967e87135a217b61fdd2075
Jonathan <jonathan.a.dias@gmail.com>
2018-06-14 23:06:17 -0300

    Ultimas Correções

--- app/src/main/java/jonathan/exerciciorestaurante/MainActivity.java
+++ app/src/main/java/jonathan/exerciciorestaurante/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edt1, edt2, edt3, edt4, edt5, edt6;
-    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,30 +19,42 @@ public class MainActivity extends AppCompatActivity {
         edt1 = (EditText)findViewById(R.id.edt_consumo);
         edt2 = (EditText)findViewById(R.id.edt_couvert_artistico);
         edt3 = (EditText)findViewById(R.id.edt_dividir);
-        bt = (Button)findViewById(R.id.bt_calcular);
         edt4 = (EditText)findViewById(R.id.edt_servico);
         edt5 = (EditText)findViewById(R.id.edt_conta_total);
         edt6 = (EditText)findViewById(R.id.edt_valor_pessoa);
 
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Double consumo, artistico, taxa, contatotal, valorpessoa, soma;
-                int npessoas;
-                consumo = Double.parseDouble(edt1.getText().toString());
-                artistico = Double.parseDouble(edt2.getText().toString());
-                npessoas = Integer.parseInt(edt3.getText().toString());
-                soma = consumo+artistico;
-                taxa = soma*0.1;
-                contatotal = soma+taxa;
-                valorpessoa = contatotal/npessoas;
+    }
 
-                edt4.setText(taxa.toString());
-                edt5.setText(contatotal.toString());
-                edt6.setText(valorpessoa.toString());
+    public void Calcular(View view) {
+
+        if(edt1.getText().toString().equals("") || edt2.getText().toString().equals("") ||
+                edt3.getText().toString().equals("")) {
+            Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_LONG).show();
+
+        }else{
+            Double consumo, artistico, taxa, contatotal, valorpessoa, soma;
+            int npessoas;
+            consumo = Double.parseDouble(edt1.getText().toString());
+            artistico = Double.parseDouble(edt2.getText().toString());
+            npessoas = Integer.parseInt(edt3.getText().toString());
+
+            if(npessoas == 0){
+                Toast.makeText(this, "O número de pessoas deve ser maior que 0!",
+                        Toast.LENGTH_LONG).show();
+            }else {
+
+                soma = consumo + artistico;
+                taxa = soma * 0.1;
+                contatotal = soma + taxa;
+                valorpessoa = contatotal / npessoas;
+
+                edt4.setText(String.format("%.2f",taxa));
+                edt5.setText(String.format("%.2f",contatotal));
+                edt6.setText(String.format("%.2f",valorpessoa));
 
             }
-        });
+
+        }
 
     }
 
